554cd9dffdaccfcd070389759a16e2490e904a3f
athabrani <sarbrani.athallah@gmail.com>
2024-12-05 17:29:48 +0700

    feat : MobileNav

--- src/components/contents/WorkSection.tsx
+++ src/components/contents/WorkSection.tsx
@@ -3,7 +3,7 @@ import { jakarta } from "@/styles/fonts";
 
 export default function CaraKerja() {
     return (
-        <section className={`${jakarta.className} flex items-center justify-center px-8 pb-12`}>
+        <section className={`${jakarta.className} flex items-center justify-center px-8 pb-12 bg-[url('/Element.png')]`}>
             <div className="relative flex items-center justify-center w-1/2">
     
                 <div className="relative rounded-full p-4">
@@ -12,14 +12,15 @@ export default function CaraKerja() {
                         alt="Work-Together"
                         width={585}
                         height={545}
+                        className=""
                     />
                 </div>
             </div>
 
             {/* Bagian Kanan: Teks */}
             <div className="w-1/2 pl-12">
-                <h1 className="relative font-semibold text-gray-800 mb-4 text-6xl md:text-5xl font-bold text-gray-800 leading-snug z-10">
-                    Bagaimana cara kerjanya?
+                <h1 className="relative font-semibold text-gray-800 mb-4 lg:text-6xl xl:text-6xl md:text-4xl sm:text-3xl text-2xl font-bold text-gray-800 leading-snug z-10">
+                    Bagaimana cara <br/> kerjanya?
                 </h1>
                 <div className="relative mt-4 z-0">
                 <Image
@@ -30,7 +31,7 @@ export default function CaraKerja() {
                     className="absolute left-36 bottom-1 md:left-12 md:bottom-0"
                     />
                 </div>
-                <ul className="text-lg text-gray-600 space-y-2 mt-8 md:text-md">
+                <ul className="text-sm sm:text-base text-gray-600 space-y-2 mt-8 md:text-md">
                     <li>Tempel kartu akses pada pemindai untuk membuka loker.</li>
                     <li>
                         Masukkan barang yang ingin disimpan ke dalam loker.
