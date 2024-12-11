8f90c35dfcf9423721c377185387a90c093f3b86
Citra Kusumadewi Sribawono <citrakusumadewisribawono@gmail.com>
2024-12-11 11:36:39 +0700

    fix sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,37 +1,46 @@
-import  Link  from "next/link";
+import Link from "next/link";
 import React from "react";
 import { FiHome, FiSettings, FiClock } from "react-icons/fi";
 
 const Sidebar: React.FC = () => {
   return (
-    <div className="bg-[#3650A2] max-h-screen text-white w-10 sm:w-10 md:w-20 flex flex-col justify-between items-center p-6 ml-10 mt-10 rounded-3xl">
+    <div className="bg-[#3650A2] min-h-screen text-white w-10 sm:w-10 md:w-20 flex flex-col justify-between items-center p-6 ml-10 mt-10 rounded-3xl">
       {/* Bagian Navigasi */}
       <nav className="space-y-6">
         <Link
-          href="/home"
-          className="flex flex-col items-center text-sm hover:bg-[#1D2D44] p-3 rounded-lg"
+          href="/dashboard"
+          className="group flex flex-col items-center text-sm p-3 rounded-lg transition duration-300"
         >
-          <FiHome size={24} />
-          <span className="mt-1 hidden md:block text-xs font-semibold">
-            Home
+          <FiHome
+            size={24}
+            className="transition duration-300 transform group-hover:scale-125"
+          />
+          <span className="mt-1 hidden md:block text-xs font-semibold transition duration-300 transform group-hover:scale-110">
+            Beranda
           </span>
         </Link>
         <Link
-          href="/clock"
-          className="flex flex-col items-center text-sm hover:bg-[#1D2D44] p-3 rounded-lg"
+          href="/riwayat"
+          className="group flex flex-col items-center text-sm p-3 rounded-lg transition duration-300"
         >
-          <FiClock size={24} />
-          <span className="mt-1 hidden md:block text-xs font-semibold">
-            Clock
+          <FiClock
+            size={24}
+            className="transition duration-300 transform group-hover:scale-125"
+          />
+          <span className="mt-1 hidden md:block text-xs font-semibold transition duration-300 transform group-hover:scale-110">
+            Riwayat
           </span>
         </Link>
         <Link
           href="/settings"
-          className="flex flex-col items-center text-sm hover:bg-[#1D2D44] p-3 rounded-lg"
+          className="group flex flex-col items-center text-sm p-3 rounded-lg transition duration-300"
         >
-          <FiSettings size={24} />
-          <span className="mt-1 hidden md:block text-xs font-semibold">
-            Settings
+          <FiSettings
+            size={24}
+            className="transition duration-300 transform group-hover:scale-125"
+          />
+          <span className="mt-1 hidden md:block text-xs font-semibold transition duration-300 transform group-hover:scale-110">
+            Pengaturan
           </span>
         </Link>
       </nav>
@@ -40,11 +49,14 @@ const Sidebar: React.FC = () => {
       <div className="flex flex-col items-center">
         {/* Garis Horizontal */}
         <div className="w-full h-[1px] bg-white mb-1"></div>
-        <Link href="/" className="mt-4 mb-2 hover:bg-[#1D2D44]">
+        <Link
+          href="/"
+          className="group mt-4 mb-2 rounded-lg"
+        >
           <img
-            src="/logout.png" // Ganti dengan path gambar Anda
+            src="/logout.png"
             alt="Logout"
-            className="w-6 h-6 md:w-8 md:h-8"
+            className="w-6 h-6 md:w-8 md:h-8 transition duration-300 transform group-hover:scale-125"
           />
         </Link>
       </div>
